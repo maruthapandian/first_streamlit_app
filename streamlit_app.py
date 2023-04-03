9a3245d6cdcd119553928e01ac3630a4d6fb1ce1
Marutha <pandixbo@gmail.com>
2023-04-03 14:28:07 -0400

    added code

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,10 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_cho
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #display the normalized data in screen
 streamlit.dataframe(fruityvice_normalized)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur=my_cnx.cursor
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row=my_cur.fetchone()
+streamlit.text("hello from snowflake:")
+streamlit.tex(my_data_row)
